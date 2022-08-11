6aaba0fd813886e5dc0e937cf75ee6a2c57d2d61
Arshak <arshakmohammed004@gmail.com>
2022-08-11 12:31:57 +0530

    url error fixed

--- src/url.js
+++ src/url.js
@@ -1,7 +1,7 @@
 import { API_KEY } from './constants/constants'
 export const trending = `trending/all/day?api_key=${API_KEY}`
-export const originals = `discover/tv?api_key=${API_KEY}&with_nerwork=213`
+export const originals= `discover/tv?api_key=${API_KEY}&with_networks=213`
 export const actions = `discover/movie?api_key=${API_KEY}&with_genres=28`
-export const comedy = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
-export const horror = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
-export const romance = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`
+export const horror = `discover/movie?api_key=${API_KEY}&with_genres=27`
+export const romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`
